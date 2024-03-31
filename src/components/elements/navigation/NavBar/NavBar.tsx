e65a3006526ef9a0ea655a9420082cb9f30e23ee
Sammy <35559259+SammyhDev@users.noreply.github.com>
2024-03-31 16:48:01 +0100

    Update NavBar.tsx

--- src/components/elements/navigation/NavBar/NavBar.tsx
+++ src/components/elements/navigation/NavBar/NavBar.tsx
@@ -5,9 +5,7 @@ import NAV_LINKS from './paths';
 const NavBar = () => {
   return (
     <HStack gap={'15px'}>
-       <div className="w-full h-20 bg-emerald-800 sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
+       
       {NAV_LINKS.map((link) => (
         <NavItem key={`link-${link.label}`} {...link} />
       ))}
