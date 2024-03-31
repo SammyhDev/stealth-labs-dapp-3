58503cbb3d5452c47f5d95608e4bbf4b7e5e81c2
Sammy <35559259+SammyhDev@users.noreply.github.com>
2024-03-31 17:17:03 +0100

    Update NavBar.tsx

--- src/components/elements/navigation/NavBar/NavBar.tsx
+++ src/components/elements/navigation/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <nav className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed top-0">
       {/* Add your Logo component here */}
-      <ul className="md:flex gap-x-6 text-white">
+      <ul className="md:flex gap-x-6 text-white hidden"> {/* Hide on small screens */}
         {NAV_LINKS.map((link) => (
           <NavItem key={`link-${link.label}`} {...link} />
         ))}
