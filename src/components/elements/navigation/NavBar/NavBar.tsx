e6d316cf4200e7850374a619b01e4255de02481d
Sammy <35559259+SammyhDev@users.noreply.github.com>
2024-03-31 16:55:41 +0100

    Update NavBar.tsx

--- src/components/elements/navigation/NavBar/NavBar.tsx
+++ src/components/elements/navigation/NavBar/NavBar.tsx
@@ -4,30 +4,20 @@ import NAV_LINKS from './paths';
 
 const NavBar = () => {
   return (
-    
-  );
-};
-
-
-
-    <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            
-            <ul className="hidden md:flex gap-x-6 text-white">
-              {NAV_LINKS.map((link) => (
-        <NavItem key={`link-${link.label}`} {...link} />
-      ))}
-            </ul>
-            
-          </div>
+    <div className="w-full h-20 bg-emerald-800 sticky top-0">
+      <div className="container mx-auto px-4 h-full">
+        <div className="flex justify-between items-center h-full">
+          {/* Add your Logo component here */}
+          <ul className="hidden md:flex gap-x-6 text-white">
+            {NAV_LINKS.map((link) => (
+              <NavItem key={`link-${link.label}`} {...link} />
+            ))}
+          </ul>
+          {/* Add your Button component here */}
         </div>
       </div>
-    </>
-
-
-
-
+    </div>
+  );
+};
 
 export default NavBar;
