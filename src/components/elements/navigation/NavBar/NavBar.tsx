0bce1e17185161491806ccc23fa54cb96bd8b65e
Sammy <35559259+SammyhDev@users.noreply.github.com>
2024-04-01 22:11:17 +0100

    Update NavBar.tsx

--- src/components/elements/navigation/NavBar/NavBar.tsx
+++ src/components/elements/navigation/NavBar/NavBar.tsx
@@ -1,19 +1,47 @@
 
 import { NavItem } from '../NavItem';
-import NAV_LINKS from './paths';
 
-const NavBar = () => {
+const MENU_LIST = [
+  { text: "Home", href: "/" },
+  { text: "About Us", href: "/about" },
+  { text: "Contact", href: "/contact" },
+];
+const Navbar = () => {
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
+
   return (
-    <nav className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed top-0">
-      {/* Add your Logo component here */}
-      <ul className="md:flex gap-x-6 text-white hidden"> {/* Hide on small screens */}
-        {NAV_LINKS.map((link) => (
-          <NavItem key={`link-${link.label}`} {...link} />
-        ))}
-      </ul>
-      {/* Add your Button component here */}
-    </nav>
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"}>
+          <a>
+            <h1 className="logo">CodeWithMarish</h1>
+          </a>
+        </Link>
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
   );
 };
 
-export default NavBar;
+export default Navbar;
